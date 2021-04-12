1d543c81228fb04020050e1f3328a57abe671924
Marina Oliveira <marinaoliv4@gmail.com>
2021-04-12 12:02:20 -0300

    Create, update e destroy

--- instagram.js
+++ instagram.js
@@ -4,6 +4,44 @@ const { Op } = require('sequelize');
 
 // Usuario
 
+//CREATE
+// Usuario.create({
+//     nome: 'mariana',
+//     email: 'mariana@email.com',
+//     senha: 'mariana'
+
+// }).then((resultado) => {
+//     console.log(resultado.toJSON());
+// });
+
+
+//alterando o email de sergio
+// Usuario.update({
+//     email: 'sergio@digitalhouse.com'
+// }, {
+//     where: {
+//         id: 2
+//     }
+// }).then((resultado) => {
+//     console.log(resultado);
+// })
+
+
+// DESTROY 
+// apagado o usuario felipe
+// Usuario.destroy({
+//     where: {
+//         id: 3
+//     }
+// }).then((resultado) => {
+//     console.log(resultado);
+// });
+
+// LISTA OS USUARIOS
+Usuario.findAll().then((usuarios) => {
+    console.log(usuarios.map((usuario) => usuario.toJSON()));
+})
+
 // usuario com letra "a" no nome
 // Usuario.findAll({
 //     where: {
@@ -30,15 +68,15 @@ const { Op } = require('sequelize');
 //buscar comentarios e exibir 2 por vez
 //exemplo abaixo feito por herbert 
 
-for (let i = 0; i < 6; i += 2) {
-    Comentario.findAll({
-      order: [['id', 'DESC']],
-      offset: i,
-      limit: 2,
-    }).then((resultado) => {
-      console.table(resultado.map((comment) => comment.toJSON()));
-    });
-  }
+// for (let i = 0; i < 6; i += 2) {
+//     Comentario.findAll({
+//       order: [['id', 'DESC']],
+//       offset: i,
+//       limit: 2,
+//     }).then((resultado) => {
+//       console.table(resultado.map((comment) => comment.toJSON()));
+//     });
+//   }
 
 // Comentario.findAll({
 //     order: [
@@ -61,6 +99,18 @@ for (let i = 0; i < 6; i += 2) {
 
 
 //post
+//criando um post relacionado ao meu usuario
+Post.create({
+  texto: 'estudando sequelize!',
+  usuarios_id: 14,
+  n_likes: 1,
+}).then((resultado) => {
+  console.log(resultado.toJSON());
+});
+
+// Post.findAll().then((posts) => {
+//     console.log(posts_id.map((posts) => post.toJSON()));
+// })
 
 // Post.findAll({
 //     where: {
